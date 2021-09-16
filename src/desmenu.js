781e62e78a6891dda1a6d081ab4c5f1f7ef50bfa
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-09-16 01:38:34 -0500

    Update desmenu.js

--- src/desmenu.js
+++ src/desmenu.js
@@ -1,35 +1,25 @@
 const desmenu = (prefix, pushname) => {
-    return `
-*Comandos De Descargas ✅*
- 
-══════════════
+    return `*┍━━━━╝✹DESCARGAS✹╚━━━━┑*
 
-*AUDIO*
+┏━━━━━━━━━━━━━━━━━━━━ 
+┣*🎵AUDIO🎶*
+┣☛ ${prefix}play2
+┃Una alternativa por si *play llego a su limite de canciones.
+┣☛ ${prefix}play
+┃Coloca el nombre de la cancion, o el link de YouTube.
+┃_Ejm_
+┣☛ ${prefix}play Faded
+┣━━━━━━━━━━━━━━━━━━━━ 
+┣*📽️VIDEO📹*
+┣☛ ${prefix}ytmp4
+┃Descarga videos de YouTube
+┃_Ejm_
+┣☛ ${prefix}ytmp4 www.youtube.com/xxxxxxxx
+┣━━━━━━━━━━━━━━━━━━━━ 
+┃_*play y *play2 tiene un limite de 2000 canciones, y se restablece cada 24 horas_
+╰━━━━━━━━━━━━━━━━━━━━   
 
-${prefix}play2
-Una alternativa por si *play llego a su limite de canciones
-
-${prefix}play
-Coloca el nombre de la cancion, o el link del video YT
-
-Ejemplo:
-${prefix}play Industry Baby - Lil Nas X
-
-══════════════
-
-*VIDEO*
-
-${prefix}ytmp4
-Descarga videos de YouTube
-
-Ejemplo:
-${prefix}ytmp4 www.youtube.com/xxxxxxxx
-
-══════════════
-    
-_*play y *play2 tiene un limite de 2000 canciones en el server, si llega a su limite se restablecera dentro de 24 horas_
-     
-ву ѕнαη∂υу`
+*BY:〲⎠⎠ɪʀᴇɴ〴 ᭄ Y🌹⃢⃟ꦼW҉𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽⁩*`
 
 }
 
